69537f11bf5c4d0750c2759829c374ed4fd1a143
Abubakar-Meigag <113248042+Abubakar-Meigag@users.noreply.github.com>
2025-05-04 14:41:35 +0100

    add VideoProvider into main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { VideoProvider } from './context/VideoContext.tsx';
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <VideoProvider>
+      <App />
+  </VideoProvider>            
+);
